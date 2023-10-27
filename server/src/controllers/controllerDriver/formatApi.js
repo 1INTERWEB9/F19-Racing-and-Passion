8a92b15ff5d512e04add267d3d4f6f9f79757554
INTERWEB <bencito10@gmail.com>
2023-10-27 09:34:45 -0500

    [ADD] format  image from API

--- server/src/controllers/controllerDriver/formatApi.js
+++ server/src/controllers/controllerDriver/formatApi.js
@@ -1,17 +1,20 @@
 const formatApi = async ({ register, Team }) => {
   const [codeDriver, numberDriver, descriptionDriver] =
     formatCodeNumberDescription({ register });
+  const imageDriver = formatImage({ register });
   let teamsFilter = [];
   if (register?.teams) {
     let teams = register?.teams.replace(/ /g, "").split(",");
     teamsFilter = await formatTeam({ Team, teams });
   }
+
   const driverFilter = filterDriver({
     register,
     teamsFilter,
     numberDriver,
     codeDriver,
     descriptionDriver,
+    imageDriver,
   });
   return driverFilter;
 };
@@ -40,12 +43,22 @@ const formatCodeNumberDescription = ({ register }) => {
   return [codeDriver, numberDriver, descriptionDriver];
 };
 
+const formatImage = ({ register }) => {
+  let imageBy = "N/A";
+  let urlImage =
+    "https://media.discordapp.net/attachments/1145765718227435656/1166468813689192488/11-racing-512.png?ex=654a99bd&is=653824bd&hm=3c32fff6cbb3a7ee6301dc8dfbc910e682977ad902fff07abd5e5fcd499b4835&=&width=432&height=432";
+  if (register.image.imageby) imageBy = register.image.imageby;
+  if (register?.image?.url !== "") urlImage = register?.image?.url;
+  return { imageBy: imageBy, urlImage: urlImage };
+};
+
 const filterDriver = ({
   register,
   teamsFilter,
   numberDriver,
   codeDriver,
   descriptionDriver,
+  imageDriver,
 }) => {
   let driverFilter = {};
   for (const key in register) {
@@ -61,6 +74,9 @@ const filterDriver = ({
         break;
       case "description":
         driverFilter[key] = descriptionDriver;
+        break;
+      case "image":
+        driverFilter[key] = imageDriver;
         break;
       default:
         driverFilter[key] = register[key];
