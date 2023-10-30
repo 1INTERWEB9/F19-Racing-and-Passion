d77de08e8d4d339f3c0536af284585e80b125613
INTERWEB <bencito10@gmail.com>
2023-10-30 18:14:44 -0500

    [ADD] custom button

---
+++ client/src/components/customButton/CustomButton.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+const CustomButton = ({ style, onClick, text, link, className, disabled }) => {
+  return (
+    <>
+      {!link
+        ? onClick.map((button, index) => (
+            <button
+              key={index}
+              onClick={button}
+              style={style}
+              className={className ? className : undefined}
+              disabled={disabled ? disabled : false}
+            >
+              {text[index]}
+            </button>
+          ))
+        : link.map((route, index) => (
+            <Link key={index} to={route}>
+              <button key={index} onClick={onClick[index]} style={style}>
+                {text[index]}
+              </button>
+            </Link>
+          ))}
+    </>
+  );
+};
+export default CustomButton;
