0c6775fbe0d2e747106ed622e499a5c30de9f3fe
INTERWEB <bencito10@gmail.com>
2023-10-31 15:45:24 -0500

    [ADD] laxy loading components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
+import { Suspense, lazy } from "react";
 import "./App.css";
-import Cards from "./components/cards/Cards";
-import DetailCard from "./components/detailCard/DetailCard";
+// import Cards from "./components/cards/Cards";
+// import DetailCard from "./components/detailCard/DetailCard";
 import { Route, Routes } from "react-router-dom";
+
+const Cards = lazy(() => import("./components/cards/Cards"));
+const DetailCard = lazy(() => import("./components/detailCard/DetailCard"));
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Cards></Cards>} />
-        <Route path="/driver/:id" element={<DetailCard />} />
+        <Route
+          path="/"
+          element={
+            <Suspense>
+              <Cards />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/driver/:id"
+          element={
+            <Suspense>
+              <DetailCard />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
