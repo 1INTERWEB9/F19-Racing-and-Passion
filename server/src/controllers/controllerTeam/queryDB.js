9b51d4a3af6b2bb37a3a8b0da5e8a74ddb3cc63f
INTERWEB <bencito10@gmail.com>
2023-11-02 08:06:26 -0500

    [UPDATE] optimizate code

--- server/src/controllers/controllerTeam/queryDB.js
+++ server/src/controllers/controllerTeam/queryDB.js
@@ -1,18 +1,14 @@
 const { Op } = require("sequelize");
 const { Team } = require("../../db");
 
-const queryDB = async ({ filters, page, pageSize, id }) => {
-  let pageDB = page + 1;
-  if (pageDB < 1) pageDB = 0;
+const queryDB = async ({ condition, id }) => {
   let { rows, count } = await Team.findAndCountAll({
     where: {
       id_Team: id ? id : { [Op.ne]: null },
       nameTeam: {
-        [Op.iLike]: filters.name,
+        [Op.iLike]: condition.name ? condition.name : "%%",
       },
     },
-    offset: page * 9,
-    limit: pageSize,
   });
   const results = rows;
   return [results, count];
--- server/src/controllers/controllerTeam/readTeam.js
+++ server/src/controllers/controllerTeam/readTeam.js
@@ -1,31 +1,14 @@
 const { queryDB } = require("./queryDB");
 
 const readTeam = async ({ condition, id }) => {
-  let filters = {
-    name: "%%",
-  };
-  let page = 0;
-  let pageSize = 9;
-  for (const key in condition) {
-    if (key.toLowerCase() != "page" && key.toLowerCase() != "pageSize")
-      filters[key] = condition[key];
-    else if (key.toLowerCase() == "page") {
-      if (condition[key] >= 1) page = condition[key] - 1;
-    } else {
-      if (condition[key] >= 1) pageSize = condition[key];
-    }
-  }
   const [results, count] = await queryDB({
-    filters,
-    page,
-    pageSize,
+    condition,
     id,
   });
   if (results.length < 1) throw new Error("Filtro invalido");
   if (results.length > 1) {
     const information = {
       count: count,
-      pages: Math.ceil(count / pageSize),
     };
     return { information, data: results };
   } else {
