6717c9034c25a82c9cf71abfe31d4c45f67b2242
INTERWEB <bencito10@gmail.com>
2023-11-04 09:47:01 -0500

    [UPDATE] form validations

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -2,83 +2,214 @@
 import css from "./form.module.css";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { GetSingleDriver } from "../../redux/actions";
+import {
+  GetSingleDriver,
+  GetTeams,
+  GetNationalities,
+} from "../../redux/actions";
 import validation from "./validation";
 import CustomInput from "../customInput/CustomInput";
+import CustomSelect from "../customSelect/CustomSelect";
+import axios from "axios";
 
-export default function LogIn({ logIn }) {
-  const [newDriver, setNewDriver] = useState({
-    driverRef: "",
-    number: null,
-    code: "",
-    forename: "",
-    surname: "",
-    imageBy: "",
-    urlImage: "",
-    dob: "",
-    nationality: "",
-    url: "",
-    teams: [],
-    description: "",
-  });
-  const [driverNationality, setDriverNationality] = useState([]);
-  const [driverTeams, setDriverTeams] = useState([]);
-  const [driverInputs, setDriverInputs] = useState([]);
+export default function Form() {
+  const [newDriver, setNewDriver] = useState({});
+  const [teamsDriver, setTeamsDriver] = useState("");
   const [errors, setErros] = useState({});
   const driver = useSelector((state) => state.singleDriver);
+  const teams = useSelector((state) => state.teams);
+  const nationalities = useSelector((state) => state.nationalities);
   const dispatch = useDispatch();
 
-  const handleChangeDriver = (event) => {
-    setNewDriver({
-      ...newDriver,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleNamesInputs = ({ driver }) => {
-    for (const key in driver) {
-      setDriverInputs([...driverInputs, key]);
-    }
-  };
-
   useEffect(() => {
-    if (user.username !== "" || user.password !== "") {
-      const userValidated = validation(user);
-      setErros({ ...userValidated });
+    if (Object.keys(newDriver).length >= 1) {
+      const driverValidated = validation(newDriver, handleInputsDriver());
+      setErros({ ...driverValidated });
     }
-    dispatch(GetSingleDriver(1));
-    handleChangeDriver(driver);
-  }, [user]);
+    if (Object.keys(newDriver).length < 1) {
+      dispatch(GetSingleDriver(1));
+      dispatch(GetTeams());
+      dispatch(GetNationalities());
+    }
+  }, [newDriver]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    logIn(user);
+    axios
+      .post(`http://localhost:3001/drivers`, newDriver)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(() => {
+        console.log("AY JOAQUIN");
+      });
+  };
+
+  const handleChangeDriver = (event) => {
+    if (event.target.name) {
+      setNewDriver({
+        ...newDriver,
+        [event.target.name]: event.target.value,
+      });
+    } else {
+      setNewDriver({
+        ...newDriver,
+        nationality: event.target.value,
+      });
+    }
+  };
+
+  const handleChangeTeam = (event) => {
+    setTeamsDriver(event.target.value);
+  };
+
+  const handleInputsDriver = () => {
+    let inputs = [];
+    for (const key in driver) {
+      if (typeof driver[key] != "object" && key != "id") {
+        inputs.push(key);
+      } else if (typeof driver[key] == "object") {
+        Object.entries(driver[key]).filter(([subkey, value]) => {
+          if (
+            !subkey.includes("id") &&
+            !subkey.includes("Nationality") &&
+            subkey.length > 2
+          )
+            inputs.push(subkey);
+        });
+      }
+    }
+    return inputs;
+  };
+
+  const handleNameTeams = () => {
+    let nameTeams = teams.map((team) => team?.nameTeam);
+    nameTeams.unshift("");
+    for (let index = 0; index < newDriver?.teams?.length; index++) {
+      nameTeams = nameTeams.filter((team) => team != newDriver?.teams[index]);
+    }
+    nameTeams.sort();
+    return nameTeams;
+  };
+
+  const handleNameNationalities = () => {
+    const nameNationalities = nationalities.map(
+      (nameNationalities) => nameNationalities?.nameNationality
+    );
+    nameNationalities.unshift("");
+    nameNationalities.sort();
+    return nameNationalities;
+  };
+
+  const handleAddTeamDriver = () => {
+    if (teamsDriver != "") {
+      let teams = newDriver?.teams ? newDriver?.teams : [];
+      teams.push(teamsDriver);
+      setNewDriver({
+        ...newDriver,
+        teams: teams,
+      });
+      setTeamsDriver("");
+    }
+  };
+  const handleRemoveTeamDriver = () => {
+    let filterTeams = newDriver?.teams;
+    filterTeams.pop();
+    setNewDriver({
+      ...newDriver,
+      teams: filterTeams,
+    });
+    handleNameTeams();
+  };
+
+  const handleErrorsInputs = () => {
+    let error = false;
+    Object.keys(errors).map((key) => {
+      errors[key] != undefined ? (error = true) : null;
+    });
+    return error;
   };
 
   return (
     <div className={css.div_custom}>
-      <form style={{ margin: 15 }} onSubmit={handleSubmit}>
-        <CustomInput
-          css={css}
-          htmlFor={["username", "password"]}
-          valuesLabel={["Username", "Password"]}
-          valuesInput={[user.username, user.password]}
-          onChanges={handleChangeDriver}
-          types={["text", "password"]}
-          errors={errors}
-        />
-        <button
-          style={{ margin: 10 }}
-          type="submit"
-          disabled={
-            !user.username ||
-            !user.password ||
-            errors?.username ||
-            errors?.password
-          }
+      <h1>Crea tu conductor</h1>
+      <form
+        style={{ margin: 15, display: "flex", flexWrap: "wrap" }}
+        onSubmit={handleSubmit}
+      >
+        <div
+          style={{
+            margin: 15,
+            display: "flex",
+            flexWrap: "wrap",
+            width: "700px",
+          }}
         >
-          Registrar
-        </button>
+          {handleInputsDriver()?.length > 1 ? (
+            <>
+              <CustomInput
+                css={css}
+                htmlFor={handleInputsDriver()}
+                valuesLabel={handleInputsDriver()}
+                valuesInput={newDriver}
+                onChange={handleChangeDriver}
+                errors={errors}
+                types={[
+                  "text",
+                  "number",
+                  "text",
+                  "text",
+                  "text",
+                  "text",
+                  "url",
+                  "date",
+                  "url",
+                  "text",
+                ]}
+              />
+            </>
+          ) : null}
+        </div>
+        <div>
+          <div style={{ width: "400px", height: "250px" }}>
+            <h2>Nacionalidad</h2>
+            <CustomSelect
+              className={css.custom_input}
+              onClick={handleChangeDriver}
+              values={handleNameNationalities()}
+              text={handleNameNationalities()}
+            />
+            {errors?.nationality && (
+              <h2 style={{ color: "red" }}>{errors.nationality}</h2>
+            )}
+          </div>
+          <div style={{ width: "400px", height: "250px" }}>
+            <h2>Equipos / Escuderos</h2>
+
+            <CustomSelect
+              className={css.custom_input}
+              onClick={handleChangeTeam}
+              values={handleNameTeams()}
+              text={handleNameTeams()}
+            />
+            <button onClick={handleAddTeamDriver}>+</button>
+            <button onClick={handleRemoveTeamDriver}>-</button>
+            {errors?.teams && <h2 style={{ color: "red" }}>{errors.teams}</h2>}
+            <p>{newDriver?.teams?.toString()}</p>
+            <div style={{ marginTop: "75px", marginBottom: "auto" }}>
+              <button
+                style={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+                type="submit"
+                disabled={handleErrorsInputs()}
+              >
+                Registrar
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
--- client/src/components/form/validation.js
+++ client/src/components/form/validation.js
@@ -1,11 +1,85 @@
-const validation = (user) => {
-  const errors = {};
-  if (!/^[a-z][^\W_]{7,14}$/i.test(user.username))
-    errors.username = "No es un usuario válido";
-  if (!/^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/.test(user.password))
-    errors.password = "No es una contraseña válida";
+import validator from "validator";
 
+const validation = (newDriver, inputs) => {
+  let keys = [...inputs, "nationality", "teams"];
+  let errors = {};
+  for (let index = 0; index < keys.length; index++) {
+    if (
+      newDriver[keys[index]] == "" ||
+      newDriver[keys[index]] == null ||
+      newDriver[keys[index]].length < 1
+    )
+      errors[keys[index]] = "Te hace falta llenar " + keys[index];
+    else {
+      switch (keys[index]) {
+        case "forename":
+          errors[keys[index]] = filterLength(
+            newDriver[keys[index]],
+            keys[index],
+            3,
+            15
+          );
+          errors[keys[index]] = filterStraightString(newDriver[keys[index]]);
+          break;
+        case "surname":
+          errors[keys[index]] = filterLength(
+            newDriver[keys[index]],
+            keys[index],
+            3,
+            15
+          );
+          errors[keys[index]] = filterStraightString(newDriver[keys[index]]);
+          break;
+        case "driverRef":
+          errors[keys[index]] = filterLength(
+            newDriver[keys[index]],
+            keys[index],
+            3,
+            20
+          );
+          break;
+        case "code":
+          if (newDriver[keys[index]].length != 3)
+            errors[keys[index]] = keys[index] + " debe ser de 3  caracteres";
+          else {
+            errors[keys[index]] = filterStraightString(newDriver[keys[index]]);
+          }
+
+          break;
+        case "description":
+          errors[keys[index]] = filterLength(
+            newDriver[keys[index]],
+            keys[index],
+            10,
+            3000
+          );
+          break;
+        case "url":
+          if (!validator.isURL(newDriver[keys[index]]))
+            errors[keys[index]] = "URL invalida";
+          break;
+        case "urlImage":
+          if (!validator.isURL(newDriver[keys[index]]))
+            errors[keys[index]] = "URL invalida";
+          break;
+        default:
+          break;
+      }
+    }
+  }
   return errors;
+};
+
+const filterStraightString = (string) => {
+  const regex = /^[a-zA-Z\-]+$/;
+  if (!regex.test(string)) return "no debe contener simbolos ni numeros";
+};
+
+const filterLength = (input, key, min, max) => {
+  if (input.length > max)
+    return key + " debe tener maximo " + max + " caracteres";
+  else if (input.length < min)
+    return key + " debe tener minimo " + min + " caracteres";
 };
 
 export default validation;
