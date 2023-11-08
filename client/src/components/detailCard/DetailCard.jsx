3ede5762089706613745eb374aa451ce1ddf199d
INTERWEB (Juan Castro) <105866006+1INTERWEB9@users.noreply.github.com>
2023-11-08 11:10:43 -0500

    [UPDATE] dispatch actions and styles

--- client/src/components/detailCard/DetailCard.jsx
+++ client/src/components/detailCard/DetailCard.jsx
@@ -1,73 +1,123 @@
 /* eslint-disable react/prop-types */
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { GetSingleDriver } from "../../redux/actions";
+import {
+  GetSingleDriver,
+  EnableWaitPage,
+  CleanCharacters,
+} from "../../redux/actions";
 import css from "./detailCard.module.css";
+import CustomLoading from "../customLoading/customLoading";
+import { FaCakeCandles } from "react-icons/fa6";
+import { BsPersonBadgeFill } from "react-icons/bs";
+import { CiBarcode } from "react-icons/ci";
+import { BiLinkExternal } from "react-icons/bi";
+import { AiOutlineFieldNumber } from "react-icons/ai";
 
 const DetailCard = () => {
   let { id } = useParams();
-  const [color, setColor] = useState({
-    "--color-state": "inherit",
-  });
+
   const driver = useSelector((state) => state.singleDriver);
   const waitPage = useSelector((state) => state.waitPage);
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(EnableWaitPage());
     dispatch(GetSingleDriver(id));
+    return () => {
+      dispatch(EnableWaitPage());
+      dispatch(CleanCharacters());
+    };
   }, [id]);
 
-  const handleColorPick = (color) => {
-    console.log(color);
-    setColor({
-      "--color-state": color,
-    });
-  };
   return (
-    <>
-      <div className={css.div_custom} hidden={waitPage}>
-        <h1 style={color} className={css.div_tittle}>
+    <div className={css.div_custom}>
+      <div hidden={waitPage}>
+        <h1 className={css.div_tittle}>
           {driver?.name?.forename ? driver?.name?.forename : driver?.forename}{" "}
           {driver?.name?.surname ? driver?.name?.surname : driver?.surname}
         </h1>
-
-        <div className={css.img} style={{ display: "flex" }}>
+        <div className={css.contain} style={{ display: "flex" }}>
           <LazyLoadImage
             className={css.img}
-            style={{
-              display: "flex",
-              marginLeft: "auto",
-              marginRight: "auto",
-              marginBottom: 10,
-              maxHeight: "auto",
-              maxWidth: "300px",
-            }}
             src={
               driver?.image?.urlImage
                 ? driver?.image?.urlImage
-                : driver?.Images?.urlImage
+                : driver?.Image?.urlImage
             }
             alt={driver.driverRef}
             effect="blur"
           />
-          <div style={{ marginLeft: 5, display: "flex", flexWrap: "wrap" }}>
-            {driver?.teams?.map((team) => (
-              <h2
-                key={team?.id_Team}
-                style={{ marginLeft: 15, marginRight: 15 }}
-              >
-                {team?.nameTeam}
+          <div className={css.book}>
+            <h1 style={{ textAlign: "center" }}>Datos</h1>
+            <h2 className={css.data}>
+              <CiBarcode /> {driver?.code ? driver?.code : "Desconocido"}
+            </h2>
+            <h2 className={css.data}>
+              <AiOutlineFieldNumber />{" "}
+              {driver?.number ? driver?.number : "Desconocido"}
+            </h2>
+            <h2 className={css.data}>
+              <BsPersonBadgeFill />{" "}
+              {driver?.driverRef ? driver?.driverRef : "Desconocido"}
+            </h2>
+            <h2 className={css.data}>
+              <FaCakeCandles /> {driver?.dob}
+            </h2>
+            <a href={driver?.url}>
+              <h2 className={css.data}>
+                <BiLinkExternal /> Mas Información
               </h2>
-            ))}
+            </a>
+          </div>
+          <div className={css.book}>
+            <h1 style={{ textAlign: "center" }}>Escuderos</h1>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+              {driver?.teams?.map((team) => (
+                <h2
+                  key={team?.id_Team}
+                  style={{
+                    marginLeft: 15,
+                    marginRight: 5,
+                    fontSize: "x-large",
+                  }}
+                >
+                  {team?.nameTeam}
+                </h2>
+              ))}
+              {driver?.Teams?.map((team) => (
+                <h2
+                  key={team?.id_Team}
+                  style={{
+                    marginLeft: 15,
+                    marginRight: 15,
+                    fontSize: "xx-large",
+                  }}
+                >
+                  {team?.nameTeam}
+                </h2>
+              ))}
+            </div>
           </div>
         </div>
-        <div style={{}}>
-          <p>{driver?.description}</p>
+        <div style={{ width: "100%", overflow: "hidden" }}>
+          <h2
+            style={{
+              marginLeft: 40,
+              marginRight: 40,
+              textAlign: "justify",
+              lineHeight: 1.2,
+            }}
+          >
+            {driver?.description}
+          </h2>
         </div>
       </div>
-      <h1 hidden={!waitPage}>Cargando...</h1>
-    </>
+      <div hidden={!waitPage}>
+        <CustomLoading />
+      </div>
+    </div>
   );
 };
 
